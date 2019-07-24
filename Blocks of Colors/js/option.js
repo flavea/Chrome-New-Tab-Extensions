e2f212481302c1b7067f051b530266fc94bc19d6
Ilma Arifiany <ilma@fabelio.com>
2019-07-25 00:22:23 +0700

    Block of Colors

---
+++ Blocks of Colors/js/option.js
@@ -0,0 +1,140 @@
+$(ready =>  {
+
+    function buildSettingJSON() {
+        let options = {}
+
+        options.name = $("#name").val()
+        options.image = $("#image").val()
+        options.color1 = $("#color1").val()
+        options.color2 = $("#color2").val()
+        options.color3 = $("#color3").val()
+        options.onlineStatus = $("#optOnline").prop("checked")
+        options.showQuote = $("#optMovie").prop("checked")
+        options.timeFormat = $("#time").val()
+        options.dateFormat = $("#date").val()
+        options.links = []
+
+        if ($('#optLinks > .link').length > 0) {
+            $('#optLinks > .link').each((i, e) => {
+                let link = {}
+                link.icon = $('.icon', e).val()
+                link.text = $('.text', e).val()
+                link.link = $('.link', e).val()
+                if(link.text != '' && link.link != '' && link.icon != '') options.links.push(link)
+            })
+        }
+
+        return options
+    }
+
+    function parseOptions(options) {
+        $("#optLinks > .link").remove()
+        $("#name").val(options.name)
+        $("#image").val(options.image)
+        $("#color1").val(options.color1)
+        $("#color2").val(options.color2)
+        $("#color3").val(options.color3)
+        $("#time").val(options.timeFormat).change()
+        $("#date").val(options.dateFormat).change()
+        if (options.onlineStatus) $("#optOnline").prop("checked", true)
+        if (options.showQuote) $("#optMovie").prop("checked", true)
+
+        if (options.links.length > 0) {
+            options.links.forEach(e => {
+                if ($('#optLinks > .link').length < 24) {
+                    let link = $('.linkTemp').clone().removeClass('linkTemp').addClass('link')
+                    $('.icon', link).val(e.icon)
+                    $('.text', link).val(e.text)
+                    $('.link', link).val(e.link)
+                    $('legend', link).text(`Link ${$('#optLinks > .link').length + 1}`)
+                    $(".delete", link).click(e => {
+                        $(e.target).parent().remove()
+                        $('#optLinks > .link').each((i, e) => {
+                            $('legend', e).text(`Link ${i+1}`)
+                        })
+                    })
+                    $("#optLinks").append(link)
+                }
+            })
+        }
+    }
+
+    $("#addLink").click(e => {
+        if ($('#optLinks > .link').length < 24) {
+            let link = $('.linkTemp').clone().removeClass('linkTemp').addClass('link')
+            $('input', link).val('')
+            $('legend', link).text(`Link ${$('#optLinks > .link').length + 1}`)
+            $("#optLinks").append(link)
+
+            $(".delete", link).click(e => {
+                $(e.target).parent().remove()
+                $('#optLinks > .link').each((i, e) => {
+                    $('legend', e).text(`Link ${i+1}`)
+                })
+            })
+        } else alert("Maximum 24 links!")
+    })
+
+    $("#save").click(e => {
+        chrome.storage.sync.set({
+            HLROptions: buildSettingJSON()
+        }, then => {
+            alert("Options saved!")
+        })
+    })
+
+    $("#export").click(e => {
+        var element = document.createElement('a')
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify({
+            "HLROptions": buildSettingJSON()
+        })))
+        element.setAttribute('download', `block_of_colors_settings_${Date.now()}.json`)
+
+        element.style.display = 'none'
+        document.body.appendChild(element)
+
+        element.click()
+
+        document.body.removeChild(element)
+    })
+
+    $("#import").click(e => {
+        $("#importFile").click()
+    })
+
+    $("#importFile").change(evt => {
+        var file = evt.target.files[0]
+
+        var reader = new FileReader()
+
+        reader.onload = (f =>  {
+            return function (e) {
+                let read = JSON.parse(e.target.result)
+                if (typeof read.HLROptions != "undefined") {
+                    let options = read.HLROptions
+                    parseOptions(options)
+                    alert('Settings imported! Do not forget to save the settings!')
+                } else alert("File format is wrong.")
+            }
+        })(file)
+
+        reader.readAsText(file)
+    })
+
+    chrome.storage.sync.get(['HLROptions'], (result) => {
+
+        if (typeof result.HLROptions == "undefined") {
+            let request = $.ajax({
+                url: 'settings.json'
+            })
+
+            request.done(data =>  {
+                options = data.HLROptions
+                parseOptions(options)
+            })
+        } else {
+            options = result.HLROptions
+            parseOptions(options)
+        }
+    })
+})
---
+++ Blocks of Colors/js/script.js
@@ -0,0 +1,93 @@
+var timeFormat = 12
+var dateFormat = 1
+
+function time() {
+	if(timeFormat == 24) $('#header h1').html($.format.date(new Date(), "HH:mm:ss"))
+	else $('#header h1').html($.format.date(new Date(), "hh:mm a"))
+	if(dateFormat == 1) $('#header span').html($.format.date(new Date(), "ddd, MMMM D yyyy"))
+	else $('#header span').html($.format.date(new Date(), "ddd, dd MMMM yyyy"))
+}
+
+function processSetting(options) {
+	let root = document.documentElement
+
+	root.style.setProperty('--color-1', `#${options.color1}`)
+	root.style.setProperty('--color-2', `#${options.color2}`)
+	root.style.setProperty('--color-3', `#${options.color3}`)
+
+	$("#name").text(`Welcome${options.name != '' ? ', ' + options.name : ''}`)
+	if (options.image != '') $("#sidebar").css("background-image", `url(${options.image})`)
+	if (options.onlineStatus) {
+		$("#online").text(`You are ${navigator.onLine ? "online" : "offline"}!`)
+	}
+	if (options.showQuote) {
+		var settings = {
+			"crossDomain": true,
+			"url": "https://andruxnet-random-famous-quotes.p.rapidapi.com/?count=1&cat=movies",
+			"method": "POST",
+			"headers": {
+				"X-RapidAPI-Host": "andruxnet-random-famous-quotes.p.rapidapi.com",
+				"X-RapidAPI-Key": "c9ed88a6d7msh2bb16885ae42db6p18398djsn14a875b76406",
+				"Content-Type": "application/x-www-form-urlencoded"
+			},
+			"data": ""
+		}
+
+		$.ajax(settings).done(function (response) {
+			response.forEach(quote => {
+				$('#movie blockquote').text(quote.quote)
+				$('#movie source').text(quote.author)
+			});
+		});
+	} else $('#movie').hide()
+
+	if (options.links.length > 0) {
+		let links = ''
+		options.links.forEach(e => {
+			links += `<a href="${e.link}">${e.icon}<span>${e.text}</span></a>`
+		})
+		$("#links").html(links)
+	}
+
+	timeFormat = options.timeFormat
+	dateFormat = options.dateFormat
+
+	setInterval(time, 1000)
+}
+
+$(ready =>  {
+	setInterval(time, 1000)
+
+	$("#go-options").click(e => {
+		if (chrome.runtime.openOptionsPage) {
+			chrome.runtime.openOptionsPage();
+		} else {
+			window.open(chrome.runtime.getURL('options.html'));
+		}
+	})
+
+	chrome.storage.sync.get(['HLROptions'], (result) => {
+		let options = {}
+		if (typeof result.HLROptions == "undefined") {
+			let request = $.ajax({
+				url: 'settings.json'
+			})
+
+			request.done(data =>  {
+				options = data.HLROptions
+				processSetting(options)
+			})
+		} else {
+			options = result.HLROptions
+			processSetting(options)
+		}
+
+	})
+
+
+	if (!navigator.onLine) {
+		$('body').addClass('offline')
+	} else {
+		$('body').removeClass('offline')
+	}
+});
