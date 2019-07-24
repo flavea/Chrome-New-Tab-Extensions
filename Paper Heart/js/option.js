5564eb9fcaecb898ab784ef742a11a867aab4775
Ilma Arifiany <ilma@fabelio.com>
2019-07-25 00:21:40 +0700

    Paper Heart

---
+++ Paper Heart/js/option.js
@@ -0,0 +1,135 @@
+$(ready =>  {
+
+    function buildSettingJSON() {
+        let options = {}
+
+        options.twitter = $("#twitter").val()
+        options.image = $("#image").val()
+        options.backgroundPosition = $("#backgroundPost").val()
+        options.color1 = $("#color1").val()
+        options.color2 = $("#color2").val()
+        options.showBookmarks = $("#optBookmarks").prop("checked")
+        options.showHistory = $("#optHistory").prop("checked")
+        options.grayScale = $("#optGrayscale").prop("checked")
+        options.timeFormat = $("#time").val()
+        options.dateFormat = $("#date").val()
+        options.links = []
+
+        if ($('#optLinks > .link').length > 0) {
+            $('#optLinks > .link').each((i, e) => {
+                let link = {}
+                link.text = $('.text', e).val()
+                link.link = $('.link', e).val()
+                if(link.text != '' && link.link != '') options.links.push(link)
+            })
+        }
+
+        return options
+    }
+
+    function parseOptions(options) {
+        $("#optLinks > .link").remove()
+        $("#twitter").val(options.twitter)
+        $("#backgroundPost").val(options.backgroundPosition)
+        $("#image").val(options.image)
+        $("#color1").val(options.color1)
+        $("#color2").val(options.color2)
+        $("#time").val(options.timeFormat).change()
+        $("#date").val(options.dateFormat).change()
+        if (options.showBookmarks) $("#optBookmarks").prop("checked", true)
+        if (options.grayScale) $("#optGrayscale").prop("checked", true)
+        if (options.showHistory) $("#optHistory").prop("checked", true)
+
+        if (options.links.length > 0) {
+            options.links.forEach(e => {
+                let link = $('.linkTemp').clone().removeClass('linkTemp').addClass('link')
+                $('.text', link).val(e.text)
+                $('.link', link).val(e.link)
+                $('legend', link).text(`Link ${$('#optLinks > .link').length + 1}`)
+                $(".delete", link).click(e => {
+                    $(e.target).parent().remove()
+                    $('#optLinks > .link').each((i, e) => {
+                        $('legend', e).text(`Link ${i+1}`)
+                    })
+                })
+                $("#optLinks").append(link)
+            })
+        }
+    }
+
+    $("#addLink").click(e => {
+        let link = $('.linkTemp').clone().removeClass('linkTemp').addClass('link')
+        $('input', link).val('')
+        $('legend', link).text(`Link ${$('#optLinks > .link').length + 1}`)
+        $("#optLinks").append(link)
+
+        $(".delete", link).click(e => {
+            $(e.target).parent().remove()
+            $('#optLinks > .link').each((i, e) => {
+                $('legend', e).text(`Link ${i+1}`)
+            })
+        })
+    })
+
+    $("#save").click(e => {
+        chrome.storage.sync.set({
+            HLROptions3: buildSettingJSON()
+        }, then => {
+            alert("Options saved!")
+        })
+    })
+
+    $("#export").click(e => {
+        var element = document.createElement('a')
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify({
+            "HLROptions3": buildSettingJSON()
+        })))
+        element.setAttribute('download', `paper_heart_settings_${Date.now()}.json`)
+
+        element.style.display = 'none'
+        document.body.appendChild(element)
+
+        element.click()
+
+        document.body.removeChild(element)
+    })
+
+    $("#import").click(e => {
+        $("#importFile").click()
+    })
+
+    $("#importFile").change(evt => {
+        var file = evt.target.files[0]
+
+        var reader = new FileReader()
+
+        reader.onload = (f => {
+            return e => {
+                let read = JSON.parse(e.target.result)
+                if (typeof read.HLROptions3 != "undefined") {
+                    let options = read.HLROptions3
+                    parseOptions(options)
+                    alert('Settings imported! Do not forget to save the settings!')
+                } else alert("File format is wrong.")
+            }
+        })(file)
+
+        reader.readAsText(file)
+    })
+
+    chrome.storage.sync.get(['HLROptions3'], (result) => {
+        if (typeof result.HLROptions3 == "undefined") {
+            let request = $.ajax({
+                url: 'settings.json'
+            })
+
+            request.done(data =>  {
+                options = data.HLROptions3
+                parseOptions(options)
+            })
+        } else {
+            options = result.HLROptions3
+            parseOptions(options)
+        }
+    })
+})
---
+++ Paper Heart/js/script.js
@@ -0,0 +1,123 @@
+var timeFormat = 12
+var dateFormat = 1
+
+function time() {
+	if(timeFormat == 24) $('#header h1').html($.format.date(new Date(), "HH:mm:ss"))
+	else $('#header h1').html($.format.date(new Date(), "hh:mm a"))
+	if(dateFormat == 1) $('#header span').html($.format.date(new Date(), "ddd, MMMM D yyyy"))
+	else $('#header span').html($.format.date(new Date(), "ddd, dd MMMM yyyy"))
+}
+
+function processSetting(options) {
+	let root = document.documentElement
+
+	root.style.setProperty('--color-1', `#${options.color1}`)
+	root.style.setProperty('--color-2', `#${options.color2}`)
+
+	$("#name").text(`Welcome, ${options.name}`)
+	if (options.image != '') $("body").css("background-image", `url(${options.image})`).css("background-position", options.backgroundPosition)
+
+	if (options.links.length > 0) {
+		let links = ''
+		options.links.forEach((e, i) => {
+			links += `<a href="${e.link}"><b>${(i+1) < 10 ? ('0'+(i+1)) : (i+1)} <span></span> </b>${e.text}</a>`
+		})
+		$("#links").html(links)
+	}
+
+	if (options.grayScale) $('.twitter-inside').addClass('grayscale')
+
+	if (options.twitter == '' && !options.showHistory) $('#content').remove()
+	if (options.twitter != '') $('.twitter-timeline').attr("href", options.twitter).attr("data-link-color", `#${options.color1}`)
+	if (options.showHistory) {
+		chrome.history.search({
+			text: '',
+			maxResults: 20
+		}, data =>  {
+			$("#history .history").remove()
+			data.forEach(page => {
+				let link = $('.historyTemp').clone().removeClass('historyTemp').addClass('history')
+				$('h3', link).text(page.title)
+				$('span', link).text(page.url)
+				$(link).attr("href", page.url)
+				$("#history").append(link)
+				$('.twitter-timeline').attr("href", options.twitter).attr("data-link-color", `#${options.color1}`)
+			});
+		});
+
+		$('#searchHistory').on('input', then => {
+			let input = $("#searchHistory").val()
+			if (input.length > 3) {
+				chrome.history.search({
+					text: input,
+					maxResults: 20
+				}, data =>  {
+					$("#history .history").remove()
+					data.forEach(page => {
+						let link = $('.historyTemp').clone().removeClass('historyTemp').addClass('history')
+						$('h3', link).text(page.title)
+						$('span', link).text(page.url)
+						$(link).attr("href", page.url)
+						$("#history").append(link)
+					});
+				});
+			}
+		});
+	}
+
+	if (options.twitter != '' && !options.showHistory) $("#twitter, [target=twitter]").show()
+	if (options.twitter == '' && options.showHistory) $("#history, [target=history]").show()
+	if (options.twitter != '' && options.showHistory) {
+		$("#twitter, .info a").show()
+		$(".info > a").click(e => {
+			$(".info > a").removeClass('active')
+			$(e.target).addClass('active')
+			let target = $(e.target).attr('target')
+			$('.ccontent').hide()
+			$(`#${target}`).show()
+
+		})
+	}
+
+	timeFormat = options.timeFormat
+	dateFormat = options.dateFormat
+
+	setInterval(time, 1000)
+}
+
+$(ready =>  {
+	setInterval(time, 1000)
+
+	$("#go-options").click(e => {
+		if (chrome.runtime.openOptionsPage) {
+			chrome.runtime.openOptionsPage();
+		} else {
+			window.open(chrome.runtime.getURL('options.html'));
+		}
+	})
+
+	chrome.storage.sync.get(['HLROptions3'], (result) => {
+		let options = {}
+		if (typeof result.HLROptions3 == "undefined") {
+			let request = $.ajax({
+				url: 'settings.json'
+			})
+
+			request.done(data =>  {
+				options = data.HLROptions3
+				processSetting(options)
+			})
+		} else {
+			options = result.HLROptions3
+			processSetting(options)
+		}
+	})
+
+
+	if (!navigator.onLine) {
+		$('body').addClass('offline')
+	} else {
+		$('body').removeClass('offline')
+		$("#twitter, [target=twitter]").hide()
+	}
+});
